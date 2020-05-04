a18400ecd794d7a606bd9d002487a106cdd8e2a0
Vivek <kumar.vivek2696@gmail.com>
2020-05-05 02:21:22 +0530

    [Pip Package] add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bizarre',
+    version='1.0.0',
+    description='A python wrapper for tor to browse sites anonymously',
+    url='git@github.com:vivekkumar2696/tor-wrapper-python.git',
+    author='Vivek Kumar',
+    author_email='kumar.vivek2696@gmail.com',
+    license='unlicense',
+    packages=['bizarre'],
+    zip_safe=False
+)
