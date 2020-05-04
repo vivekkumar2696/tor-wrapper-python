955dd3e528335050d3c1c73d8e959befc91e7242
Vivek <kumar.vivek2696@gmail.com>
2020-05-05 02:29:20 +0530

    [Pip Package] install requirements.txt for setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name='bizarre',
     version='1.0.0',
@@ -9,5 +12,6 @@ setup(
     author_email='kumar.vivek2696@gmail.com',
     license='unlicense',
     packages=['bizarre'],
-    zip_safe=False
+    zip_safe=False,
+    install_requires=required
 )
